d01c9f69f4024bb96c4cc2114fd880b7dfacc949
Alyson Pereira <alyson.bosch.senai14@gmail.com>
2026-03-08 22:21:21 -0300

    Upload File 08/03 - feat: Learning and have fun with exercise of traffic light on Streamlit

---
+++ 3- Streamlit/01.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import streamlit as st
+
+st.title("Data Analytics Platform")
+st.subheader("Name: Alyson Pereira dos Santos")
+data = 'print("Hello World")'
+
+st.code(data)
+
+def trafficLight():
+    traffic_light = ['red', 'yellow', 'green']
+
+    choose = st.text_input('Type the traffic light color')
+
+    if st.button('Check Avaliability'):
+        have_it = choose.lower() in traffic_light
+        'We have this traffic light ' if have_it else 'We don\'t have this traffic light'
+        return True
+    else:
+        print("We don't have this traffic light!")
+        return False
+    
+check = st.button('Exercise 01')
+close = ''
+
+if check:
+    trafficLight()
+    close = st.button('Close')
+    
+    
